2387f1e9af48ce9f97c1eb59582dcc907a193859
notbowen <contact@hubowen.dev>
2025-05-29 07:42:09 +0000

    feat: add NVIDIA support

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -29,6 +29,9 @@
   # WSL Configuration
   wsl.enable = true;
   wsl.defaultUser = "bowen";
+  wsl.useWindowsDriver = true;
+  nixpkgs.config.allowUnfree = true;
+
 
   # Set hostname
   networking.hostName = "nixos";
@@ -68,6 +71,43 @@
       PermitRootLogin = "no";
       PasswordAuthentication = false;
     };
+  };
+
+  # NVIDIA on Docker
+  services.xserver.videoDrivers = ["nvidia"];
+  hardware.nvidia.open = true;
+  
+  environment.sessionVariables = {
+      CUDA_PATH = "${pkgs.cudatoolkit}";
+      EXTRA_LDFLAGS = "-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib";
+      EXTRA_CCFLAGS = "-I/usr/include";
+      LD_LIBRARY_PATH = [
+          "/usr/lib/wsl/lib"
+          "${pkgs.linuxPackages.nvidia_x11}/lib"
+          "${pkgs.ncurses5}/lib"
+  ];
+      MESA_D3D12_DEFAULT_ADAPTER_NAME = "Nvidia";
+  };
+  
+  hardware.nvidia-container-toolkit = {
+      enable = true;
+      mount-nvidia-executables = false;
+  };
+  
+  systemd.services = {
+      nvidia-cdi-generator = {
+          description = "Generate nvidia cdi";
+          wantedBy = [ "docker.service" ];
+          serviceConfig = {
+          Type = "oneshot";
+          ExecStart = "${pkgs.nvidia-docker}/bin/nvidia-ctk cdi generate --output=/etc/cdi/nvidia.yaml --nvidia-ctk-path=${pkgs.nvidia-container-toolkit}/bin/nvidia-ctk";
+          };
+      };
+  };
+  
+  virtualisation.docker = {
+      daemon.settings.features.cdi = true;
+      daemon.settings.cdi-spec-dirs = ["/etc/cdi"];
   };
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
